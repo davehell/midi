34725485ac52c58367f34dee88b8929c51070357
David Hellebrand <david@hellebrand.eu>
2014-01-28 20:58:54 +0100

    stahovani ukazek

--- subdomeny/midi2/js/main.js
+++ subdomeny/midi2/js/main.js
@@ -3,3 +3,9 @@ $('form#stazeniSkladby button#stahnout').click(function() {
   var url = $('form#stazeniSkladby #format').val();
   if(url) window.location = url;
 });
+
+//stazeni ukazky skladby po kliknuti na tlacitko #stahnoutDemo
+$('button#stahnoutDemo').click(function() {
+  var url = $('#formatDema').val();
+  if(url) window.location = url;
+});
